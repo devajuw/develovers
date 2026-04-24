683f538d39ce3003b350f7c847cc880b422cc4fd
Dev Raj <devcuber45@gmail.com>
2026-04-24 22:22:16 +0530

    try express json

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,24 +3,27 @@ const express = require('express')
 const connectDB = require ("./Config/database")
 const app = express()
 const User = require("./models/user")
+app.use(express.json())
 app.post("/signup", async (req, res) => {
 // creating a new instance of the User Model
-const user = new User({
-    firstName: "Dev",
-    lastName : "Raj",
-    emailId : "devvib@gmail.com",
-    password : "ijhajhd9",
-})
-try {
-await user.save()
-res.send("User added successfully!!")
-} catch(err){
-    res.status(40).send("User NOY ADDED !!!"+ errr.message)
-}
+console.log(req.body)
+// const user = new User({
+//     firstName: "Dev",
+//     lastName : "Raj",
+//     emailId : "devvib@gmail.com",
+//     password : "ijhajhd9",
+// })
+// try {
+// await user.save()
+// res.send("User added successfully!!")
+// } catch(err){
+//     res.status(40).send("User NOY ADDED !!!"+ errr.message)
+// }
 
+// res.send("Signup route is working")
 })
 
-connectDB().then (() =>{
+connectDB().then (async () =>{
     console.log("database conncted✅")
     app.listen(3000,() => {
         console.log("The server is Running at PORT 3000")
