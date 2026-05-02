c364a8cfd78747c73da74985d7a69fe1c43c288e
Dev Raj <devcuber45@gmail.com>
2026-05-02 22:25:26 +0530

    random

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,80 +1,79 @@
-require('dotenv').config()
-const express = require('express')
-const connectDB = require ("./Config/database")
-const app = express()
-const User = require("./models/user")
-app.use(express.json())
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./Config/database");
+const app = express();
+const User = require("./models/user");
+app.use(express.json());
 
 // User SignUP
 
 app.post("/signup", async (req, res) => {
   try {
-    const user = new User(req.body)
-    await user.save()
-    res.send("User added successfully!!")
-  } catch(err){
-    res.status(400).send("User NOT ADDED: " + err.message)
-  }
-})
-
-app.get("/GetUser", async (req, res) => {
-  const userEmail = req.query.userEmail
-  try {
-    const user = await User.find({})
-    //empty filter means searching all users
-    if (!user || user.length === 0)
-      res.status(400).send("User not found")
-    else
-      res.send(user);
-    console.log(user);
-  }
-  
-  catch (err) {
-    res.status(400).send("Something went wrong: " + err.message)
+    const user = new User(req.body);
+    await user.save();
+    res.send("User added successfully!!");
+  } catch (err) {
+    res.status(400).send("User NOT ADDED: " + err.message);
   }
 });
 
+app.get("/GetUser", async (req, res) => {
+  const userEmail = req.query.userEmail;
+  try {
+    const user = await User.find({});
+    //empty filter means searching all users
+    if (!user || user.length === 0) res.status(400).send("User not found");
+    else res.send(user);
+    console.log(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong: " + err.message);
+  }
+});
 
 // deleting the user
 
 app.delete("/user", async (req, res) => {
-  const userId = req.body.userId
+  const userId = req.body.userId;
   try {
     const user = await User.findByIdAndDelete(userId);
-    res.send("User Deleted Successfully")
-  }
-  catch (err) {
-    res.status(400).send("Something went wrong")
+    res.send("User Deleted Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
 // UPDATE the data of the user
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
-  const data = req.body
-  console.log(data)
+app.patch("/user/:userId", async (req, res) => {
+  
+  const userId = req.params?.userId;
+  const data = req.body;
+  const ALLOWED_UPDATES = ["userId", "photoUrl", "about", "gender", "age"];
   try {
+  const isUpdateAllowed = Object.keys(data).every((k) =>
+    ALLOWED_UPDATES.includes(k)
+  );
+  if (!isUpdateAllowed) {
+    throw new Error("Update not allowed")
+  }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "before",
-      runValidators: true});
-    console.log(user)
-    res.send("User Updated Successfully")
-  }
-  catch (err) {
-    res.status(400).send("Update Failed"+err.message)
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("User Updated Successfully");
+  } catch (err) {
+    res.status(400).send("🚨Update Failed: " + err.message);
   }
 });
 
-
-
-
-
-connectDB().then (async () =>{
-    console.log("database conncted✅")
-    app.listen(3000,() => {
-        console.log("The server is Running at PORT 3000")
-    })
-}).catch(err=> {
-    console.log("database not conncted❌", err.message)
-})
+connectDB()
+  .then(async () => {
+    console.log("database conncted✅");
+    app.listen(3000, () => {
+      console.log("The server is Running at PORT 3000");
+    });
+  })
+  .catch((err) => {
+    console.log("database not conncted❌", err.message);
+  });
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,39 +1,44 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-    firstName : {
-    type: String,
-    required: true
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-    lastName : {
-        type: String
+    lastName: {
+      type: String,
     },
-    emailId :{
-        type: String,
+    emailId: {
+      type: String,
       required: true,
       unique: true,
-    lowercase: true
+      lowercase: true,
     },
-    password :{
-        type: String,
-    required: true
-    } ,
-    age :{
-        type: Number,
-    required: true
+    password: {
+      type: String,
+      required: true,
     },
-    gender : {
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
       type: String,
       validate(value) {
         if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender data is not valid")
+          throw new Error("Gender data is not valid");
         }
-      }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about section.",
+    },
   },
-  about: {
-    type: String,
-    default : "This is a default about section."
-  }
-}, { timestamps: true })
+  {
+    timestamps: true,
+  },
+);
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
